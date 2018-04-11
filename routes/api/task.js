4c82a290ac085b1e80b1c1903c2018f55f1db460
Ryan Calingo <gryan.calingo@gmail.com>
2018-04-11 08:03:20 -0400

    set up task route

--- routes/api/task.js
+++ routes/api/task.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const taskController = require("../../controllers/taskController");
 
 // Matches with "/api/task"
-router.route("/");
-//   .get(taskController.findAll)
-//   .post(taskController.create);
+router.route("/")
+  .get(taskController.findAll)
+  .post(taskController.create);
 
 // Matches with "/api/books/:id"
-// router
-//   .route("/:id")
-//   .get(taskController.findById)
-//   .put(taskController.update)
-//   .delete(taskController.remove);
+router
+  .route("/:id")
+  .get(taskController.findById)
+  .put(taskController.update)
+  .delete(taskController.remove);
 
 module.exports = router;
